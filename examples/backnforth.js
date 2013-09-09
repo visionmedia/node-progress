1e809688ffe9d84acaaaa0d7264caceedf355050
Jordan Scales <scalesjordan@gmail.com>
2013-09-09 16:25:18 -0400

    Example to both increase and decrease the value of a progress bar

---
+++ examples/backnforth.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ * Example to increase and decrease the value of a progress bar
+ * to show the functionality of `terminate()`.
+ */
+
+var ProgressBar = require('../');
+
+var bar = new ProgressBar(':title [:bar] :percent', {
+    complete: '='
+  , incomplete: ' '
+  , width: 30
+  , total: 100
+});
+
+function forward() {
+  bar.tick(1, { title: 'forward ' });
+  if (bar.curr > 60) {
+    backward();
+  } else {
+    setTimeout(forward, 20);
+  }
+}
+
+function backward() {
+  bar.tick(-1, { title: 'backward' });
+  if (bar.curr == 0) {
+    bar.terminate();
+  } else {
+    setTimeout(backward, 20);
+  }
+}
+
+forward();
