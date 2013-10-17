f090ac15fde8349a68da2ddceeb0dae856033007
Christoffer Hallas <christoffer.hallas@gmail.com>
2013-10-17 14:43:35 +0200

    lets you simply pass an fmt and a total, instead of an options object

---
+++ examples/simple.js
@@ -0,0 +1,9 @@
+var ProgressBar = require('../')
+  , bar         = new ProgressBar('[:bar]', 10);
+
+var id = setInterval(function (){
+  bar.tick();
+  if (bar.complete) {
+    clearInterval(id);
+  }
+}, 100);
