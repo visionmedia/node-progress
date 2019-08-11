7fd02dba557c4d4a7fb6e9cf1dd7b5444684ddeb
turbopope <PapstDonB@Googlemail.com>
2019-08-11 17:20:28 +0800

    Add tests for other options

--- test/node-progress.test.js
+++ test/node-progress.test.js
@@ -6,20 +6,15 @@ const CURSOR_TO_START = '\u001b[0K';
 const NEW_LINE = '\n';
 
 
-test('creates an ProgressBar object', () => {
-  let bar = new ProgressBar(':bar', { total: 1 });
-  expect(bar).toBeDefined();
-});
 
-
-const simulateProgressBar = (bar, callback, tickDuration = 20) => {
-  for (let i = 0; i < bar.total; i++) {
+const simulateProgressBar = (bar, callback, tickDuration = 50, ticks = bar.total) => {
+  for (let i = 0; i < ticks; i++) {
     setTimeout(() => bar.tick(), tickDuration * i);
   }
-  setTimeout(callback, (bar.total + 1) * tickDuration);
+  setTimeout(callback, (ticks + 1) * tickDuration);
 }
 
-describe('it should render correctly', () => {
+describe('ProgressBar', () => {
 
   let stdout = [];
   let stderr = [];
@@ -41,11 +36,47 @@ describe('it should render correctly', () => {
     unhook();
   });
 
+
+  test('creates a ProgressBar object', () => {
+    let bar = new ProgressBar(':bar', { total: 1 });
+    expect(bar).toBeDefined();
+  });
+
   test('renders the default bar correctly', (done) => {
     let bar = new ProgressBar(':bar', { total: 5 });
   
     simulateProgressBar(bar, () => {
       expect(stderr).toStrictEqual([
+        CLEAR_LINE,
+        '=----', CURSOR_TO_START, CLEAR_LINE,
+        '==---', CURSOR_TO_START, CLEAR_LINE,
+        '===--', CURSOR_TO_START, CLEAR_LINE,
+        '====-', CURSOR_TO_START, CLEAR_LINE,
+        '=====', CURSOR_TO_START, NEW_LINE
+      ]);
+      done();
+    });
+  });
+
+  test('starts at given position', (done) => {
+    let bar = new ProgressBar(':bar', { total: 5, curr: 2 });
+  
+    simulateProgressBar(bar, () => {
+      expect(stderr).toStrictEqual([
+        CLEAR_LINE,
+        '===--', CURSOR_TO_START, CLEAR_LINE,
+        '====-', CURSOR_TO_START, CLEAR_LINE,
+        '=====', CURSOR_TO_START, NEW_LINE
+      ]);
+      done();
+    }, undefined, 3);
+  });
+
+  test('renders the default bar to stdout correctly', (done) => {
+    let bar = new ProgressBar(':bar', { total: 5, stream: process.stdout });
+  
+    simulateProgressBar(bar, () => {
+      expect(stdout).toStrictEqual([
         CLEAR_LINE,
         '=----', CURSOR_TO_START, CLEAR_LINE,
         '==---', CURSOR_TO_START, CLEAR_LINE,
@@ -99,6 +130,23 @@ describe('it should render correctly', () => {
   
     simulateProgressBar(bar, () => {
       expect(stderr[stderr.length - 1]).toBe(NEW_LINE);
+      done();
+    });
+  });
+
+  test('calls callback on completion', (done) => {
+    let callback = jest.fn();
+    let bar = new ProgressBar(':bar', { total: 1, callback });
+    simulateProgressBar(bar, () => {
+      expect(callback).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  test('clears bar on completion', (done) => {
+    let bar = new ProgressBar(':bar', { total: 1, clear: true });
+    simulateProgressBar(bar, () => {
+      expect(stderr[stderr.length - 1]).toBe(CLEAR_LINE);
       done();
     });
   });
